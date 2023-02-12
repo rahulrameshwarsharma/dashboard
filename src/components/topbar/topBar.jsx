b15c6606f2a9f0b4d4bf99dc68f5d8f46a9bd5e8
rahulrameshwarsharma <91724758+rahulrameshwarsharma@users.noreply.github.com>
2023-02-13 00:57:13 +0530

    Update topBar.jsx

--- src/components/topbar/topBar.jsx
+++ src/components/topbar/topBar.jsx
@@ -6,7 +6,7 @@ export default function Topbar() {
       <div className='topbar'>
          <div className="topbarWrapper">
           <div className="topLeft">
-            <span className="logo">Intela</span>
+            <span className="logo">Touchak</span>
           </div>
 
           <div className="topRight">
